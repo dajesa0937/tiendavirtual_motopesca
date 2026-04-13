9a4a6bffaadeeb45332f2fc5c4c8bfaaae48cb6c
Dajesa <93830906+Dajesa@users.noreply.github.com>
2026-04-12 19:33:03 -0500

    ajuste tamaño tarjetas

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -4,21 +4,18 @@ function renderProducts() {
 
   products.forEach((p) => {
     container.innerHTML += `
-      <div class="bg-white rounded-xl shadow hover:shadow-xl transition overflow-hidden group">
+      <div class="product-card overflow-hidden">
 
-        <!-- IMAGEN -->
-        <div class="relative">
+        <!-- CONTENEDOR IMAGEN -->
+        <div class="w-full h-48 bg-gray-100 flex items-center justify-center overflow-hidden rounded-lg">
+
           <img src="${p.image}" 
-               class="w-full h-48 object-cover group-hover:scale-105 transition">
+               class="w-full h-full object-cover transition duration-300">
 
-          <!-- BADGE -->
-          <span class="absolute top-2 left-2 bg-[#FF7A00] text-white text-xs px-2 py-1 rounded">
-            Oferta
-          </span>
         </div>
 
         <!-- INFO -->
-        <div class="p-4">
+        <div class="mt-3">
           <h3 class="font-semibold text-sm mb-1">${p.name}</h3>
 
           <p class="text-lg font-bold text-[#0A2540]">
@@ -26,7 +23,7 @@ function renderProducts() {
           </p>
 
           <button onclick="addToCart(${p.id})"
-            class="w-full mt-3 bg-[#FF7A00] text-white py-2 rounded-lg hover:opacity-90">
+            class="w-full mt-2 bg-[#FF7A00] text-white py-2 rounded-lg hover:bg-orange-600 transition">
             Añadir al carrito
           </button>
         </div>
